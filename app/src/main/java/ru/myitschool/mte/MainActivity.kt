97a2c0b4bd138ddea983af3f0ae8b34c0fe72d8d
Pavel <impetus@yandex.ru>
2023-02-08 20:03:50 +0300

    Fixes

--- app/src/main/java/ru/myitschool/mte/MainActivity.kt
+++ app/src/main/java/ru/myitschool/mte/MainActivity.kt
@@ -1,27 +1,48 @@
-package ru.myitschool.mte;
+package ru.myitschool.mte
 
-import android.os.Bundle;
-import android.widget.Button;
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.coroutines.*
+import ru.myitschool.mte.databinding.ActivityMainBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import ru.myitschool.mte.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
-    private Button btnStart, btnStop;
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var job: Job
+    private var counter = 0
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        btnStart = binding.content.startBtn
+        btnStop = binding.content.stopBtn
+        val frManager = supportFragmentManager
 
-        btnStart = binding.content.startBtn;
-        btnStop = binding.content.stopBtn;
+        val fragments = arrayOf(FirstFragment(), ProceedingFragment())
 
+        btnStart.setOnClickListener {
+
+            // launch a new coroutine in background and continue
+
+            job = CoroutineScope(Dispatchers.IO).launch{
+                while(true) {
+                    val transaction: FragmentTransaction = frManager.beginTransaction()
+                    val currentFragment: Fragment = fragments.get(counter++ % 2)
+                    transaction.replace(R.id.output_fragment, currentFragment)
+                    transaction.commit()
+                    delay(3000)
+
+                }
+            }
+        }
+        btnStop.setOnClickListener {
+            job.cancel()
+        }
     }
 }
